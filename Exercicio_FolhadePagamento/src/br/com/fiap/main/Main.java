67c8263086ba4d73dfaf228312a8297c9ca02c24
Alexandre Faria <132949575+alefaria577@users.noreply.github.com>
2025-03-17 11:37:45 -0300

    Aula 6 - Folha_pagamento

---
+++ Exercicio_FolhadePagamento/src/br/com/fiap/main/Main.java
@@ -0,0 +1,12 @@
+package br.com.fiap.main;
+
+public class Main {
+    public int salario;
+    public int num_dependente;
+    public int descontoINSS;
+    public int plano_Saude;
+}
+
+public void Valor_Plano_Saude(){
+    plano_Saude = 100 * num_dependente;
+}
